b01c8804a2cb4fe15de6132d08926e01ff5fb483
Igor Lourenço <igorlourenco.dev@gmail.com>
2022-02-25 16:14:21 -0300

    feat: deploy food truck

---
+++ scripts/deploy-food-truck.ts
@@ -0,0 +1,34 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const FoodTruck = await ethers.getContractFactory("FoodTruck");
+  const foodTruck = await FoodTruck.deploy(
+    "0xe0533D416056d8998BA0D81c5DcF75310E25291e",
+    "0x50492D9c6B5cEe195fF89fee881b50Ff63F7CaD4",
+    "https://my-nft-minter.vercel.app/metadata"
+  );
+
+  await foodTruck.deployed();
+
+  console.log("FoodTruck deployed to:", foodTruck.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
